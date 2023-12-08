d0e5288b60eb3545d192099dc2f7ce76f8ebc5b8
Volodymyr <hohiop09@gmail.com>
2023-12-08 18:39:16 +0200

    make build

---
+++ index.13061e67.js
@@ -0,0 +1,3 @@
+const i=document.querySelectorAll(".navigation__link");function t(t){i.forEach(i=>{i.classList.contains("is-active")&&i.classList.remove("is-active")}),t.target.classList.add("is-active")}i.forEach(i=>i.addEventListener("click",t));//# sourceMappingURL=index.13061e67.js.map
+
+//# sourceMappingURL=index.13061e67.js.map
